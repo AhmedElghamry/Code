c451975e5932cf2445706e2a77b5b77007f8331c
AhmedElghamry <ahmed.elghamry511@gmail.com>
2021-05-16 22:21:52 +0200

    sim adjust

--- Simulation.java
+++ Simulation.java
@@ -10,18 +10,11 @@ public class Simulation {
 			int input = takeInput();
 			while(input != 0) {
 				if (input == 1) {
-					System.out.println("\nWrite your Name: ");
-					String name = sc.nextLine();
-					System.out.println("\nWrite your Email: ");
-					String email = sc.nextLine();
-					System.out.println("\nWrite your Password: ");
-					String password = sc.nextLine();
-					System.out.println("\nChoose: 1- Buyer | 2- Store Owner\n");
-					int in = sc.nextInt();
-					if (in == 1) { usersList.add(new Buyer(name,password,email)); }
-					else { usersList.add(new StoreOwner(name,password,email)); }
-					System.out.println("\nWelcome " + name + ", you have been registered successfully. Please, Login again!");
+					register();
 					continue;
+				}
+				if (input == 2) {
+
 				}
 			}
 	}
@@ -31,8 +24,7 @@ public class Simulation {
 		//usersList.add(new User("admin","123","admin@zc.edu.eg"));
 	}
 
-	private static int takeInput()
-	{
+	private static int takeInput() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("What do you want to do?\n 1- Register\n 2- Login\n 3- Exit\n");
 		input = scanner.nextInt();
@@ -40,6 +32,20 @@ public class Simulation {
 
 		else*/
 		return input;
+	}
+
+	private static void register() {
+		System.out.println("\nWrite your Name: ");
+		String name = sc.nextLine();
+		System.out.println("\nWrite your Email: ");
+		String email = sc.nextLine();
+		System.out.println("\nWrite your Password: ");
+		String password = sc.nextLine();
+		System.out.println("\nChoose: 1- Buyer | 2- Store Owner\n");
+		int in = sc.nextInt();
+		if (in == 1) { usersList.add(new Buyer(name,password,email)); }
+		else { usersList.add(new StoreOwner(name,password,email)); }
+		System.out.println("\nWelcome " + name + ", you have been registered successfully. Please, Login again!");
 	}
 }
 
